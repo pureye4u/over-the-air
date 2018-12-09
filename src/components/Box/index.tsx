be37bbf7d408200f49a1b289daa4dad41cd1df4e
pureye4u <pureye4u@gmail.com>
2018-12-10 08:17:41 +0900

    Init project

---
+++ src/components/Box/index.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+const Box : React.SFC = () : JSX.Element => {
+    return (
+        <div className="box">
+          BOX
+        </div>
+    );
+};
+
+export default Box;
